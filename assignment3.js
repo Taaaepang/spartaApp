5df7b1d83a36ff9fafcf79bd1d03bef4438757fe
김태광 <taegwang@gimtaegwang-ui-MacBookPro.local>
2020-08-04 00:12:11 +0900

    과제3. 이메일 검증

--- assignment3.js
+++ assignment3.js
@@ -3,24 +3,24 @@
 function checkEmail(email){
     let check = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
     if (check.test(email))
-        console.log("good")
+        console.log("이메일이 맞습니다.")
     else
-        console.log("bad")
+        console.log("이메일이 아닙니다.")
 }
 /*
 split으로 찾기.
 function checkEmail(email){
     let check = email.split('@')
     if (check == email){
-        console.log("bad")
+        console.log("이메일이 아닙니다.")
         return -1
     }
     check2 = check[1].split('.')
     if (check2 == check[1]){
-        console.log("bad")
+        console.log("이메일이 아닙니다.")
         return -1
     }
-    console.log("good")
+    console.log("이메일이 맞습니다.")
 }
 */
 
@@ -28,9 +28,9 @@ function checkEmail(email){
 indexOf로 찾기.
 function checkEmail(email){
     if(email.indexOf('@') !== -1 && email.indexOf('.') !== -1)
-        console.log("good")
+        console.log("이메일이 맞습니다.")
     else
-        console.log("bad")
+        console.log("아메일이 아닙니다.")
 }
 */
 checkEmail('ggp02261@gmail.com')
